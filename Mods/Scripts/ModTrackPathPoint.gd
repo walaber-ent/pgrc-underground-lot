807eb0a3e45477c4f11e667b0d3affdf875c530d
Tim FitzRandolph <walaber@walaber.com>
2025-02-01 20:18:06 -0800

    alt route scripts + data
    - alt route script and data

--- Mods/Scripts/ModTrackPathPoint.gd
+++ Mods/Scripts/ModTrackPathPoint.gd
@@ -8,7 +8,11 @@ func _ready() -> void:
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_TRANSFORM_CHANGED:		
-		var parent : ModTrackPath = get_parent() as ModTrackPath
-		if parent:
-			parent.refresh_mesh()
+		var parent_path : ModTrackPath = get_parent() as ModTrackPath
+		if parent_path:
+			parent_path.refresh_mesh()
+			
+		var parent_alt_route : ModTrackPathAltRoute = get_parent() as ModTrackPathAltRoute
+		if parent_alt_route:
+			parent_alt_route.refresh_mesh()
 	
